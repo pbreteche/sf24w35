92e081c7930842aef85724ff6c41f68f0b16c79b
Pierre Bretéché <pbreteche@dawan.fr>
2024-08-28 10:39:38 +0200

    User cannot vote twice for the same message

--- src/Twig/Components/Thread.php
+++ src/Twig/Components/Thread.php
@@ -6,6 +6,7 @@ use App\Entity\Issue;
 use App\Entity\Post;
 use App\Repository\PostRepository;
 use App\Repository\VoteRepository;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent]
@@ -16,6 +17,7 @@ final class Thread
     public function __construct(
         private readonly PostRepository $postRepository,
         private readonly VoteRepository $voteRepository,
+        private readonly Security $security,
     ) {
     }
 
@@ -24,11 +26,32 @@ final class Thread
      */
     public function getPosts(): array
     {
+        if (!isset($this->issue)) {
+            throw new \InvalidArgumentException('Issue parameter should not be empty');
+        }
+
         return $this->postRepository->findBy(['issue' => $this->issue], ['createdAt' => 'ASC']);
     }
 
     public function countVote(Post $post, bool $against = false): int
     {
         return $this->voteRepository->count(['message' => $post, 'against' => $against]);
+    }
+
+    public function canVote(Post $post): bool
+    {
+        $user = $this->security->getUser();
+        // Users cannot vote for their own message
+        if ($post->getCreatedBy() === $user) {
+            return false;
+        }
+
+        // Users cannot vote twice for the same message
+        $existingVote = $this->voteRepository->count(['voter' => $user, 'message' => $post]);
+        if ($existingVote) {
+            return false;
+        }
+
+        return true;
     }
 }
