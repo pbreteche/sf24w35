750b583e22f994a4c90449f9c45e79e1426279ea
Pierre Bretéché <pbreteche@dawan.fr>
2024-08-30 12:28:02 +0200

    Définir la locale utilisateur

---
+++ src/EventListener/LocaleListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+final class LocaleListener
+{
+    #[AsEventListener(event: KernelEvents::REQUEST, priority: 32)]
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        $locale = $request->getPreferredLanguage(['fr', 'en', 'de', 'es']);
+
+        if ($locale) {
+            $request->setDefaultLocale($locale);
+        }
+    }
+}
